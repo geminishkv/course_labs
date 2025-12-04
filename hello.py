45411364f165bacf9bcb78418c7635a49d4de184
Aleksandr Gorbunov <bcmactive@gmail.com>
2025-12-04 18:29:40 +0100

    Add comments to hello.py code

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
+# Приветственное приложение Hello AppSec World
 print("Hello AppSec World!")
+# Запрос имени пользователя
 name=input("Enter name:")
+# Вывод приветствия с именем
 print("Hello AppSec World from",name)
 

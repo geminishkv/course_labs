dc9cb956ff14a1dfdb6bb08b53822a3199f1db97
Aleksandr Gorbunov <bcmactive@gmail.com>
2025-12-04 18:33:46 +0100

    Refactor hello.py with improved code style

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
-# Приветственное приложение Hello AppSec World - измененный комментарий
-print("Hello AppSec World!")
-# Запрос имени пользователя
-name=input("Enter name:")
-# Вывод приветствия с именем пользователя
-print("Hello AppSec World from",name)
+#!/usr/bin/env python3
+"""
+Hello AppSec World application
+Demonstrates Python code with improved style
+"""
+
+def greet_user(username: str) -> None:
+    """Print greeting message with username"""
+    greeting_message = f"Hello AppSec World from {username}!"
+    print(greeting_message)
+
+
+if __name__ == "__main__":
+    print("Hello AppSec World!")
+    user_name = input("Enter name: ")
+    greet_user(user_name)
 
